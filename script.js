57eac42efba6e125bc4b47325e6ea7426cc452f4
Clinton Johnson <dragoon1246224@gmail.com>
2018-08-06 16:40:22 -0400

    New Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const sampleArray = [469, 755, 244, 245, 758, 450, 302, 20, 712, 71, 456, 21, 398, 339, 882, 848, 179, 535, 940, 472]; 
+
+function addElement() {
+    let number1 = document.createElement("number1");
+    node.appendChild(
+        for (let number1 = 1; number1 <= 20; number1++;)
+
+    )
+}
